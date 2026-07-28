f94c4149d5e89cd47a607d56e6a134441d2b72c9
kikudesuyo <kikuchi.hiromu.609@s.kyushu-u.ac.jp>
2026-07-29 01:48:08 +0900

    fix

--- web/src/lib/api/client.ts
+++ web/src/lib/api/client.ts
@@ -23,7 +23,7 @@ type ApiObjectResponse<T> = {
 
 
 const apiBaseUrl = (() => {
-	const rawUrl = import.meta.env.PUBLIC_API_BASE_URL || 'http://localhost:8081';
+	const rawUrl = env.PUBLIC_API_BASE_URL 
 	return rawUrl.endsWith('/api/v1') ? rawUrl : `${rawUrl}/api/v1`;
 })();
 
